9eaf587d74d7dbd39728ef72a3d7fca635beb40a
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-07-30 16:09:50 -0500

    added tests.

--- test_core.py
+++ test_core.py
@@ -32,28 +32,43 @@ def test_Property_str():
     assert p.__str__() == 'small cabin: $100 per month'
 
 
-#def test_inventory_init_():
-
-    #p = Inventory([
-        #Property('small cabin', 5, 100, 150),
-        #Property('medium cabin', 6, 150, 200),
-        #Property('large cabin', 3, 300, 400),
-        #Property('studio apartment', 4, 150, 200),
-        #Property('medium apartment', 3, 200, 300),
-        #Property('penthouse', 1, 400, 800)
-    #])
-
-    #assert p.properties == 'properties:\nsmall cabin: $100 per month\nmedium cabin: $150 per month\nlarge cabin: $300 per month\nstudio apartment: $150 per month\nmedium apartment: $200 per month\npenthouse: $400 per month'
-
-
-def test_inventory_str_():
-
-    p = Inventory([
+def test_inventory_init_():
+    properties = [
         Property('small cabin', 5, 100, 150),
         Property('medium cabin', 6, 150, 200),
         Property('large cabin', 3, 300, 400),
         Property('studio apartment', 4, 150, 200),
         Property('medium apartment', 3, 200, 300),
         Property('penthouse', 1, 400, 800)
+    ]
 
-    assert p.__str__() == 'properties:\nsmall cabin: $100 per month\nmedium cabin: $150 per month\nlarge cabin: $300 per month\nstudio apartment: $150 per month\nmedium apartment: $200 per month\npenthouse: $400 per month'
+    inv = Inventory(properties)
+
+    assert inv.properties == properties
+
+
+def test_inventory_str():
+
+    properties = [
+        Property('small cabin', 5, 100, 150),
+        Property('medium cabin', 6, 150, 200),
+        Property('large cabin', 3, 300, 400),
+        Property('studio apartment', 4, 150, 200),
+        Property('medium apartment', 3, 200, 300),
+        Property('penthouse', 1, 400, 800)
+    ]
+
+    inv = Inventory(properties)
+
+    assert inv.__str__(
+    ) == 'Properties:\nsmall cabin: $100 per month\nmedium cabin: $150 per month\nlarge cabin: $300 per month\nstudio apartment: $150 per month\nmedium apartment: $200 per month\npenthouse: $400 per month'
+
+
+def test_rental_init():
+
+    R = Rental('timothy', ['small cabin'], '3', 'rent')
+
+    assert R.name == 'timothy'
+    assert R.items == ['small cabin']
+    assert R.length == '3'
+    assert R.type == 'rent'

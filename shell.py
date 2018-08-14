874dae3f46a8f1a3d479dc1057acf61e2d94ea88
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-08-14 15:54:51 -0500

    fixed some issues within the program.

--- shell.py
+++ shell.py
@@ -1,98 +1,108 @@
-from core import *
-from disk import *
+import core
+import disk
 
 
-def user(inv, customer_employee):
+def customer(inv, customer_employee, rent_or_return):
 
+    while input != 'q' or rent_or_return == 'R':
+
+        print(inv)
+        print()
+
+        rental = int(input('Which would you like to rent today?(0, 5) '))
+        print()
+        if rental > 5:
+            break
+
+        name = input('What name would be on this rental? ')
+        print()
+
+        rental_length = input('How many months will you be renting? ')
+        print()
+
+        rent = core.Rental(name, [], rental_length, rent_or_return)
+        item = inv[rental]
+        rentals = rent.add_item(item)
+
+        print('Thanks for your business.')
+        print()
+
+        print(rent)
+        return rent
+        break
+
+
+def return_rental(inv, rent_or_return):
+    while input != 'q' or rent_or_return == 'C':
+
+        return_item = input('What rental would you like to close? ')
+        print()
+
+        customer = input('What was the name? ')
+        print()
+
+        length = input('How many months were you renting? ')
+        print()
+
+        print('Thank you for your business.')
+        print()
+
+        return_list = core.Rental(customer, [], length, rent_or_return)
+        item = inv.return_item(return_item)
+        rentals = return_list.add_item(item)
+
+        for line in inv.properties:
+            if return_item.lower() in line.name.lower():
+
+                print(return_list.return_string())
+                return return_list.return_string()
+
+        break
+
+
+def employee(customer_employee):
     while input != 'q':
-        if customer_employee == 'C':
 
-            rent_or_return = input(
-                'Would you like to [R]ent or [C]lose a current rental? ')
-            print()
-            if rent_or_return == 'R':
+        history = input(
+            'Would you like to see the transaction [H]istory or the [I]nventory? '
+        )
+        print()
 
-                print(inv)
-                print()
+        if history == 'H':
 
-                rental = int(
-                    input('Which would you like to rent today?(0, 5) '))
-                print()
+            disk.employee()
 
-                name = input('What name would be on this rental? ')
-                print()
+        elif history == 'I':
 
-                rental_length = input('How many months will you be renting? ')
-                print()
-
-                rent = Rental(name, [], rental_length, rent_or_return)
-                item = inv[rental]
-                rentals = rent.add_item(item)
-
-                print('Thanks for your business.')
-                print()
-
-                print(rent)
-                return rent
-
-            elif rent_or_return == 'C':
-
-                return_item = input('What rental would you like to close? ')
-                print()
-
-                customer = input('What was the name? ')
-                print()
-
-                length = input('How many months were you renting? ')
-                print()
-
-                print('Thank you for your business.')
-                print()
-
-                return_list = Rental(customer, [], length, rent_or_return)
-                item = inv.return_item(return_item)
-                rentals = return_list.add_item(item)
-
-                for line in inv.properties:
-                    if return_item.lower() in line.name.lower():
-
-                        print(return_list.return_string())
-                        return return_list.return_string()
-
-        elif customer_employee == 'E':
-
-            history = input(
-                'Would you like to see the transaction [H]istory or the [I]nventory? '
-            )
-            print()
-
-            if history == 'H':
-
-                employee()
-
-            elif history == 'I':
-
-                print_inventory()
+            disk.print_inventory()
         break
 
 
 def main():
 
-    inv = get_inventory()
+    inv = disk.get_inventory()
 
     print('Welcome to my rental agency press "q" to quit at any time.')
     print()
 
     customer_employee = input('Are you a [C]ustomer or an [E]mployee? ')
     print()
-
-    rent = user(inv, customer_employee)
-
-    write_to_log(rent)
+    if customer_employee == 'C':
+        rent_or_return = input(
+            'Would you like to [R]ent or [C]lose a current rental? ')
+        print()
+        if rent_or_return == 'R':
+            rent = customer(inv, customer_employee, rent_or_return)
+            disk.write_to_log(rent)
+        elif rent_or_return == 'C':
+            return_log = return_rental(inv, rent_or_return)
+            disk.write_to_log(return_log)
+    elif customer_employee == 'E':
+        employee(customer_employee)
 
     inventory = inv.update_stock()
 
-    give_inventory(inventory)
+    disk.give_inventory(inventory)
 
 
 if __name__ == '__main__':
--- test_core.py
+++ test_core.py
@@ -11,18 +11,94 @@ def test_Property_init_():
     assert p.replacement == 150
 
 
-def test_Property_rent():
+def test_small_cabin_rent():
 
     p = Property('small cabin', 5, 100, 150)
 
     assert p.rent == 100
 
 
-def test_Property_matches_name():
+def test_medium_cabin_rent():
+
+    p = Property('medium cabin', 5, 150, 200)
+
+    assert p.rent == 150
+
+
+def test_large_cabin_rent():
+
+    p = Property('large cabin', 5, 300, 400)
+
+    assert p.rent == 300
+
+
+def test_studio_apartment_rent():
+
+    p = Property('studio apartment', 5, 300, 400)
+
+    assert p.rent == 300
+
+
+def test_medium_apartment_rent():
+
+    p = Property('medium apartment', 5, 300, 400)
+
+    assert p.rent == 300
+
+
+def test_penthouse_rent():
+
+    p = Property('penthouse', 5, 300, 400)
+
+    assert p.rent == 300
+
+
+def test_small_cabin_matches_name():
 
     p = Property('small cabin', 5, 100, 150)
 
     assert p.name == 'small cabin'
+
+
+def test_medium_cabin_matches_name():
+
+    p = Property('medium cabin', 5, 100, 150)
+
+    assert p.name == 'medium cabin'
+
+
+def test_large_cabin_matches_name():
+
+    p = Property('large cabin', 5, 100, 150)
+
+    assert p.name == 'large cabin'
+
+
+def test_studio_apartment_matches_name():
+
+    p = Property('studio apartment', 5, 100, 150)
+
+    assert p.name == 'studio apartment'
+
+
+def test_Property_matches_name():
+
+    p = Property('medium apartment', 5, 100, 150)
+
+    assert p.name == 'medium apartment'
+
+
+def test_Penthouse_matches_name():
+
+    p = Property('penthouse', 5, 100, 150)
+
+    assert p.name == 'penthouse'
+
+
+def test_Property_history_str():
+    p = Property('small cabin', 5, 100, 150)
+
+    assert p.history_string() == 'small cabin, 5, 100, 150'
 
 
 def test_Property_str():
@@ -72,6 +148,13 @@ def test_rental_init():
     assert R.items == ['small cabin']
     assert R.length == '3'
     assert R.type == 'rent'
+
+
+def test_rental_return_str():
+    R = Rental('timothy', [Property('small cabin', 5, 100, 150)], '3', 'C')
+
+    assert R.return_string(
+    ) == '-----------------\nType: C\nCustomer: timothy\nDeposit: 15.0\nTotal: $321.00 for 3 months\nProperty: \nsmall cabin: $100 per month\n----------------'
 
 
 def test_rental_str():
